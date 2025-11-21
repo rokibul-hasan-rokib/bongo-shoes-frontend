9ccb5b8acca2162c3eceb4d276b5efae5032afbf
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-11-21 21:02:27 +0600

    feat: update API_BASE URL for service integration

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
 //  export const API_BASE = "http://127.0.0.1:8000/api";
-  // export const API_BASE = "https://hrhabib.pythonanywhere.com/api";
-  export const API_BASE = "https://bongoshoes.quantumcraft.cloud/api";
+  export const API_BASE = "https://hrhabib.pythonanywhere.com/api";
+  // export const API_BASE = "https://bongoshoes.quantumcraft.cloud/api";
